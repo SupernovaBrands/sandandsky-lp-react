0aa7c4376227cae690c3fb996efa0ce53e6c6e6c
Teddy Maulana <teddy.maulana@supernovabrands.com>
2022-05-27 00:48:38 +0700

    test 11

--- src/UI/templates/ProductDetail.js
+++ src/UI/templates/ProductDetail.js
@@ -2,12 +2,13 @@ import React, { lazy, Suspense } from 'react';
 import ProductForm from '../sections/ProductForm';
 import '../../product.scss';
 import Carousel from '../components/Carousel';
+import ProductImagePreview from "../components/ProductImagePreview";
 import ProductProvider from '../../store/ProductProvider';
-import FaqAccordion from '../sections/FaqAccordion';
 
 const CustomerReviews = lazy(() => import('../sections/CustomerReviews'));
 const CustomerReviews2 = lazy(() => import('../sections/CustomerReviews2'));
 const Recognize = lazy(() => import('../sections/Recognize'));
+const FaqAccordion = lazy(() => import('../sections/FaqAccordion'));
 
 const ProductDetail = (props) => {
     return (
