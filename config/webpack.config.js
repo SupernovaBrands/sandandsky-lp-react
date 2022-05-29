78f236bf510e4dd49fc0a7a4b3f6db5968d01089
Teddy Maulana <teddy.maulana@supernovabrands.com>
2022-05-29 20:13:33 +0700

    test 16

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -593,8 +593,8 @@ module.exports = function (webpackEnv) {
           {},
           {
             inject: false,
-            template: 'public/test-15.html',
-            filename: 'test-15/index.html'
+            template: 'public/test-16.html',
+            filename: 'test-16/index.html'
           },
           isEnvProduction
             ? {
