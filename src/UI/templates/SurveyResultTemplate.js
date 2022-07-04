6e642c2ff18b29f7ec80e1aa03eb1d87eacc9106
rizkiaprilian <rizki.a.aprilian@gmail.com>
2022-07-04 14:07:20 +0700

    add ref & height post

--- src/UI/templates/SurveyResultTemplate.js
+++ src/UI/templates/SurveyResultTemplate.js
@@ -1,13 +1,26 @@
 import SurveyResult from '../components/SurveyResult';
-import { getCookie } from "../../modules/Utils";
+import { getCookie, postIframeHeight } from "../../modules/Utils";
+import { useEffect, useRef } from 'react';
+import { useResizeDetector } from 'react-resize-detector';
+import { useSearchParams } from 'react-router-dom';
 
 window.getCookie = getCookie;
 
 const SurveyResultTemplate = () => {
+	const [searchParams] = useSearchParams();
+	const site = searchParams.get('site');
+
+	const targetRef = useRef();
+	const { width, height } = useResizeDetector({ targetRef });
+
+	useEffect(() => {
+		postIframeHeight('height', height, site);
+	}, [height]);
+
 	const answerResult = getCookie('surveyResult') ? JSON.parse(getCookie('surveyResult')) : {};
 
 	return (
-		<div className='overflow-hidden'>
+		<div ref={targetRef} className='overflow-hidden'>
 			<div className='row justify-content-center survey-content align-content-start'>
 				<SurveyResult answerResult={answerResult} />
 			</div>
