355202f0ec4d548edb91fc24e4c0110271b91fb2
rizkiaprilian <rizki.a.aprilian@gmail.com>
2022-07-05 09:36:46 +0700

    merge master

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -690,32 +690,33 @@ module.exports = function (webpackEnv) {
                 },
               }
             : undefined
-        ),
-        new HtmlWebpackPlugin(
-          Object.assign(
-            {},
-            {
-              inject: false,
-              template: 'public/range-apc.html',
-              filename: 'range-apc/index.html',
-            },
-            isEnvProduction
-              ? {
-                  minify: {
-                    removeComments: true,
-                    collapseWhitespace: true,
-                    removeRedundantAttributes: true,
-                    useShortDoctype: true,
-                    removeEmptyAttributes: true,
-                    removeStyleLinkTypeAttributes: true,
-                    keepClosingSlash: true,
-                    minifyJS: true,
-                    minifyCSS: true,
-                    minifyURLs: true,
-                  },
-                }
-              : undefined
-          )
+        )
+      ),
+      new HtmlWebpackPlugin(
+        Object.assign(
+          {},
+          {
+            inject: false,
+            template: 'public/range-apc.html',
+            filename: 'range-apc/index.html',
+          },
+          isEnvProduction
+            ? {
+                minify: {
+                  removeComments: true,
+                  collapseWhitespace: true,
+                  removeRedundantAttributes: true,
+                  useShortDoctype: true,
+                  removeEmptyAttributes: true,
+                  removeStyleLinkTypeAttributes: true,
+                  keepClosingSlash: true,
+                  minifyJS: true,
+                  minifyCSS: true,
+                  minifyURLs: true,
+                },
+              }
+            : undefined
+        )
       ),
       // Inlines the webpack runtime script. This script is too small to warrant
       // a network request.
