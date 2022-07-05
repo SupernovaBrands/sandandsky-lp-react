2465cc38d2fe73306925169ca32fdd10bfad7971
rizkiaprilian <rizki.a.aprilian@gmail.com>
2022-07-05 16:28:31 +0700

    fix lazyload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Suspense, lazy } from "react";
 import { Route, Routes } from 'react-router-dom';
 import Layout from './UI/layout/Layout';
-// import SurveyResultTemplate from "./UI/templates/SurveyResultTemplate";
 
 const ProductDetail = lazy(() => import('./UI/templates/ProductDetail'));
-const Survey = lazy(() => import('./UI/templates/Survey'));
 const ApcRange = lazy(() => import('./UI/templates/ApcRange'));
+const Survey = lazy(() => import('./UI/templates/Survey'));
 const SurveyResultTemplate = lazy(() => import('./UI/templates/SurveyResultTemplate'));
+
 
 const App = () => {
   const noHeader = () => window.location.pathname === '/customers-survey' || window.location.pathname === '/customers-survey/'
@@ -20,9 +20,9 @@ const App = () => {
           <Route path='/pore-tight-kit' element={<ProductDetail />} />
           <Route path='/australian-pink-clay-pore-tight-kit' element={<ProductDetail />} />
           <Route path='/test-18' element={<ProductDetail />} />
-          <Route path='/range-apc' element={<ApcRange />} />
           <Route path='/customers-survey' element={<Survey />} />
           <Route path='/survey-result' element={<SurveyResultTemplate />} />
+          <Route path='/range-apc' element={<ApcRange />} />
         </Routes>
       </Suspense>
     </Layout>
--- src/UI/templates/ApcRange.js
+++ src/UI/templates/ApcRange.js
@@ -1,15 +1,15 @@
 import { lazy, Suspense } from "react";
 import ProductProvider from "../../store/ProductProvider";
-import '../../range.scss';
 import RangeBanner from "../sections/RangeBanner";
-import RangeCarousel from "../sections/RangeCarousel";
-import RangeFormula from "../sections/RangeFormula";
 import RangeApcFAQ from "../../store/range-apc-faq";
+import '../../range.scss';
 
+const RangeCarousel = lazy(() => import('../sections/RangeCarousel'));
 const CustomerReviews = lazy(() => import('../sections/CustomerReviews'));
 const CustomerReviews2 = lazy(() => import('../sections/CustomerReviews2'));
 const Recognize = lazy(() => import('../sections/Recognize'));
 const FaqAccordion = lazy(() => import('../sections/FaqAccordion'));
+const RangeFormula = lazy(() => import('../sections/RangeFormula'));
 
 const ApcRange = () => {
 	return (
