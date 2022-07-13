0a60c5dc3ad0c02ef0f28dbf1650e5509835ed1c
rizkiaprilian <rizki.a.aprilian@gmail.com>
2022-07-13 16:48:33 +0700

    update banner v2

--- src/UI/sections/RangeBanner.js
+++ src/UI/sections/RangeBanner.js
@@ -8,8 +8,8 @@ const RangeBanner = () => {
 			</div>
 
 			<picture className="d-block w-100">
-				<source srcSet="https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/794dd288-3953-45af-848d-ea2a9d199600/2280x" media="(min-width: 768px)" type="image/webp" />
-				<source srcSet="https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/1841fccd-7ec2-4523-5dcd-78d5fa0f6f00/2280x" media="(min-width: 768px)" type="image/jpeg" />
+				<source srcSet="https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/3122223b-21a2-4bed-93cf-f442f0fe5000/2280x" media="(min-width: 768px)" type="image/webp" />
+				<source srcSet="https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/37c0aff1-a9a1-4ce0-a7ff-c7bdc4bf7600/2280x" media="(min-width: 768px)" type="image/jpeg" />
 				<source srcSet="https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/a192e3ee-849d-46d4-7ac7-633b61e9b500/828x" type="image/webp" />
 				<source srcSet="https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/69115651-1071-4dd0-c5af-b58cdae11500/828x" type="image/jpeg" />
 				<img
