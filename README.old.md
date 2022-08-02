fa6c822e48c756e8df55d418da6b7266218a50e1
rizkiaprilian <rizki.a.aprilian@gmail.com>
2022-08-02 13:05:24 +0700

    Updates

--- README.old.md
+++
@@ -1,1 +0,0 @@
-# sandandsky-lp-react
--- src/store/range-context.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const RangeContext = React.createContext({
-    price: '',
-    comparePrice: '',
-    changeStore: (activeStore, productHandle) => {},
-});
-
-export default RangeContext;
