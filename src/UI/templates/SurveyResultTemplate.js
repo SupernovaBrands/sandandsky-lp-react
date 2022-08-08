da955af942a1fdcce481b661f227c604a9bf3ac8
pebiantara <pebiantara@gmail.com>
2022-08-08 17:10:13 +0700

    [2rwp07b] update result page

--- src/UI/templates/SurveyResultTemplate.js
+++ src/UI/templates/SurveyResultTemplate.js
@@ -1,12 +1,13 @@
 import SurveyResult from '../components/SurveyResult';
 import { getCookie } from "../../modules/Utils";
 import { useRef } from 'react';
+import { useSearchParams } from "react-router-dom";
 
 window.getCookie = getCookie;
 
 const SurveyResultTemplate = () => {
-	// const [searchParams] = useSearchParams();
-	// const site = searchParams.get('site');
+	const [searchParams] = useSearchParams();
+	const site = searchParams.get('site');
 
 	const targetRef = useRef();
 	// const { width, height } = useResizeDetector({ targetRef });
@@ -20,7 +21,7 @@ const SurveyResultTemplate = () => {
 	return (
 		<div ref={targetRef} className='overflow-hidden'>
 			<div className='row justify-content-center survey-content align-content-start'>
-				<SurveyResult answerResult={answerResult} />
+				<SurveyResult answerResult={answerResult} site={site} />
 			</div>
 		</div>
 	);
