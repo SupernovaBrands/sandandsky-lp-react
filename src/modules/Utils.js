aef02ded3d9cef6ce255849e51e6b67d2aabb1a3
Teddy Maulana <teddy.maulana@supernovabrands.com>
2022-09-18 12:05:57 +0700

    updates

--- src/modules/Utils.js
+++ src/modules/Utils.js
@@ -1,5 +1,9 @@
 import { useLayoutEffect, useState } from "react";
 
+export const validateEmail = function (t) {
+	return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(String(t).toLowerCase());
+};
+
 export const Base64 = {
 	decode(str) {
 		return decodeURIComponent(
