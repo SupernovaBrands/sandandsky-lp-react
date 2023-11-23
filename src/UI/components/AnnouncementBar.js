04dfedfe7de1e41cfbe884e6fa5d7f8736884877
Awais Gill <awais@supernovabrands.com>
2023-11-23 07:28:15 +0500

    task/867967856

--- src/UI/components/AnnouncementBar.js
+++ src/UI/components/AnnouncementBar.js
@@ -1,9 +1,9 @@
 const AnnouncementBar = () => {
 	return (
-		<div id="announcementBar" className="announcement-bar bg-primary carousel slide" data-bs-ride="carousel" data-bs-interval="4000">
+		<div id="announcementBar" className="announcement-bar carousel slide" data-bs-ride="carousel" data-bs-interval="4000">
 			<div className="carousel-inner container">
 				<div className="carousel-item text-center text-white active">
-					<a href="https://www.amazon.com/sandandsky?maas=maas_adg_8EA37C43F6AD95DB249DE08F65E1DCAA_afap_abs&ref_=aa_maas&tag=maas" className="d-flex align-items-center justify-content-center font-size-sm">Prime Big Deal Days: Up to 30% off!</a>
+					<a href="https://www.amazon.com/sandandsky?maas=maas_adg_8EA37C43F6AD95DB249DE08F65E1DCAA_afap_abs&ref_=aa_maas&tag=maas" className="d-flex align-items-center justify-content-center font-size-sm">Black Friday Cyber Monday: Up to 30% off. 20-29 Nov</a>
 				</div>
 			</div>
 		</div>
